42daceb81e09a5fe6510f7b4f00422618dcf3466
ChrisDobby <dobby@chrisjdobson.co.uk>
2019-06-19 20:19:25 +0100

    Added new pluralsight link

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,15 @@ import cricket_scorer from "../assets/images/cricket-scorer.png";
 import guide_1 from "../assets/images/guide-1.png";
 import guide_2 from "../assets/images/guide-2.png";
 import guide_3 from "../assets/images/guide-3.png";
+import guide_4 from "../assets/images/guide-4.png";
 
 const GUIDES = [
+    {
+        img: guide_4,
+        url:
+            "https://www.pluralsight.com/guides/react-component-composition/",
+        description: "Component Composition in React",
+    },
     {
         img: guide_1,
         url:
